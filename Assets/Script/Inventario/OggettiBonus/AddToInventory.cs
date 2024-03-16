c4b520d23d8305134d5b94f0c800eb3fec73ea62
seorehnamuh <deborah.fazzuoli@istitutopantheon.it>
2024-03-16 16:45:23 +0100

    fixed error inventory

--- Assets/Script/Inventario/OggettiBonus/AddToInventory.cs
+++ Assets/Script/Inventario/OggettiBonus/AddToInventory.cs
@@ -12,6 +12,7 @@ public class AddToInventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.DeleteAll();
         text.text = PlayerPrefs.GetInt(nameOfPlayerPref).ToString();
     }
 
